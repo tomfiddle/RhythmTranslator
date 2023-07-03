226cb7b0f5e00b84ec9cb3833659920a8e62f6ad
tomfiddle <tatjanakim6@gmail.com>
2023-07-03 04:23:46 +0200

    added drawables for notes added buttons (have to be constrained yet) audio player doesn't pause thread anymore

--- app/src/main/java/com/example/rhythmtranslator/MainActivity.java
+++ app/src/main/java/com/example/rhythmtranslator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rhythmtranslator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -7,20 +8,19 @@ import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.LinkedHashMap;
-import java.lang.System;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,13 +29,17 @@ public class MainActivity extends AppCompatActivity {
     private TextView notesView;
     private Button convertButton;
     private ImageButton playButton;
+    private ImageButton pauseButton;
+    private ImageButton stopButton;
+    private ImageButton loopButton;
     private LinkedHashMap<Character, String> noteDictionary;
+    private LinkedHashMap<Character, Integer> noteImages;
 
     private MediaPlayer notePlayer = new MediaPlayer();
     private MediaPlayer metronome = new MediaPlayer();
 
+    private String notes;
     private boolean isPlaying = false;
-
 
 
     @SuppressLint({"MissingInflatedId"})
@@ -51,68 +55,56 @@ public class MainActivity extends AppCompatActivity {
         notesView = findViewById(R.id.notes_view);
         convertButton = findViewById(R.id.convert_button);
         playButton = findViewById(R.id.play_button);
+        pauseButton = findViewById(R.id.pause_button);
+        stopButton = findViewById(R.id.stop_button);
+        loopButton = findViewById(R.id.loop_button);
 
         //read dictionary.txt and put into linked hashmap
         getNoteToChar(noteDictionary);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                audioPlayer(notes);
+                setMetronome();
+            }
+        });
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notePlayer.pause();
+            }
+        });
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notePlayer.stop();
+            }
+        });
 
-        /* When convert button is clicked:
-        * 1. get user text
-        * 2. convert characters to notes
-        * 3. build note string
-        * 4. show notes in text view*/
+        loopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notePlayer.setLooping(true);
+            }
+        });
 
         convertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Get user text
                 String userText = userTextInput.getText().toString();
-                String notes = translateToNotes(userText); // Returns string, e. g. 0100
+                notes = translateToNotes(userText); // Returns string, e. g. 0100
                 // Display the translated letters for now
                 notesView.setText(notes);
                 audioPlayer(notes);
                 setMetronome();
             }
         });
-      /*  convertButton.setOnClickListener((new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String userText = userTextInput.getText().toString();
-                String notes = translateToNotes(userText); // Returns String
-                convertButton.setOnClickListener((new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        convertButton.setVisibility(View.INVISIBLE);
-                        notesView.setText(notes);
-                        // Add metronome with tempo 60
-                        audioPlayer(notes);
-                    }
-                }));
-                convertButton.setVisibility(View.INVISIBLE);
-                playButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                            audioPlayer(notes);
-
-                    }
-                });
-                userTextInput.setOnClickListener((new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        convertButton.setVisibility(View.VISIBLE);
-                    }
-                }));
-            }
-
-        }));
-
-        */
-
     }
 
 
-
-    private String translateToNotes(String userText){
+    @NonNull
+    private String translateToNotes(String userText) {
         StringBuilder noteText = new StringBuilder();
         userText = userText.toLowerCase();
         for (int i = 0; i < userText.length(); i++) {
@@ -121,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
             if (Character.isLetter(c) || Character.isWhitespace(c)) {
                 if (noteDictionary.containsKey(c)) {
                     String note = noteDictionary.get(c);
-                        noteText.append(note);
+                    noteText.append(note);
                 }
             } else {
                 noteText.append(c); // Append non-letter and non-whitespace characters as-is
@@ -133,69 +125,106 @@ public class MainActivity extends AppCompatActivity {
         return notes;
     }
 
-    private void getNoteToChar(LinkedHashMap<Character, String> map){
+    // Not necessary at all
+    // Wanted to separate the data from the logic
+    private void getImageToNote() {
+        AssetManager assetManager = getAssets();
+        try {
+            InputStream inputStream = assetManager.open("letterToNoteImages.txt");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Separate using "-"
+                String[] parts = line.split("-");
+
+                char letter = parts[0].charAt(0);
+                String resourceString = parts[1];
+                // Android Studio doesn't like that
+                int resourceIdNoteImages = getResources().getIdentifier(resourceString, "drawable", getPackageName());
+                noteImages.put(letter, resourceIdNoteImages);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void getNoteToChar(LinkedHashMap<Character, String> map) {
         AssetManager assetManager = getAssets();
         try {
             InputStream inputStream = assetManager.open("dictionary.txt");
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
-            String line = "";
-            while((line = reader.readLine())!=null){
+            String line;
+            while ((line = reader.readLine()) != null) {
                 // Separate using "-"
-                String [] parts = line.split("-");
+                String[] parts = line.split("-");
 
                 char letter = parts[0].charAt(0);
                 String note = parts[1];
                 // Add to hashmap
                 map.put(letter, note);
-
             }
             reader.close();
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private void displayNoteImages(){
+        LinearLayout noteImagesContainer = findViewById(R.id.note_images_container);
+        noteImagesContainer.removeAllViews();
+
+
+    }
+
     private void audioPlayer(String notes) {
+        //notePlayer = MediaPlayer.create(this, R.raw.snare);
         try {
-            AssetFileDescriptor afd = getResources().openRawResourceFd(R.raw.click);
+            AssetFileDescriptor afd = getResources().openRawResourceFd(R.raw.snare);
             notePlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
             afd.close();
             notePlayer.prepare();
+
+            final long delay = 250; // Delay for the 16th rest (250ms)
+            final long noteDuration = 250; // Duration for the notes (500ms)
+
+            final Handler handler = new Handler();
+            final Runnable playNoteRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    notePlayer.start();
+                }
+            };
+
             for (int i = 0; i < notes.length(); i++) {
                 char c = notes.charAt(i);
                 String singleNote = String.valueOf(c);
                 if (singleNote.equals("1")) {
-                    notePlayer.start();
-                    isPlaying = true;
-                } else {
-                    //long millis = System.currentTimeMillis();
-
-                    // Or set the time in a TextView for debugging purposes
-                    // textView.setText("Current Time: " + millis);
-                    // Use countDownTimer
-                    Thread.sleep(250);
+                    handler.postDelayed(playNoteRunnable, i * (delay + noteDuration));
                 }
+                notePlayer.reset();
+                notePlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
             }
         } catch (IOException e) {
             throw new RuntimeException("Failed to set note source", e);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
     }
 
 
-    private void setMetronome(){
+    private void setMetronome() {
         try {
             AssetFileDescriptor afd = getResources().openRawResourceFd(R.raw.click);
             metronome.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
             afd.close();
             metronome.prepare();
-            // If noteplayer starts playing or if playButton is pressed start playing metronome
-            if(isPlaying == true){
+            // If note player starts playing or if playButton is pressed start playing metronome
+            if (isPlaying) {
                 metronome.start();
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
